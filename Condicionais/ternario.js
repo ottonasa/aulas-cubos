2e48a9a5a1faa41750d6e7cfdf0e0694266a6a18
OTTO <otto@MacBook-Air-de-OTTO.local>
2023-07-25 18:36:08 -0300

    Usando tenario.

---
+++ Condicionais/ternario.js
@@ -0,0 +1,14 @@
+const saldo = 191479379;
+
+
+// if (saldo === 1) {
+//     console.log(`Você tem ${saldo} real`)
+// } else {
+//    console.log(`Você tem ${saldo} reais`)
+// }
+
+// Tenario
+
+const unidade = (saldo === 1 ? "real" : "reais")
+console.log(`Você tem ${saldo} ${unidade}`)
+
