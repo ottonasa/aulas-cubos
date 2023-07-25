01bcaca364ed21d753ba020f6e4ee049d7f3e05b
OTTO <otto@MacBook-Air-de-OTTO.local>
2023-07-25 15:44:32 -0300

    Subindo aulas de condicionais

---
+++ Condicionais/oper-comparacao.js
@@ -0,0 +1,22 @@
+// true
+const ehMaior = 25 > 16;
+
+console.log(ehMaior);
+{
+    // false
+    const ehMaior = 8 > 16;
+
+    console.log(ehMaior);
+
+}
+
+
+{
+    // com x e y 
+    const x = 9;
+    const y = 17;
+    const ehMaior = x <= y;
+
+    console.log(ehMaior);
+
+}
