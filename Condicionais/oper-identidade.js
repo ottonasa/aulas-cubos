2cd18862a58d36f00ea2965c2a84350593f6d3d5
OTTO <otto@MacBook-Air-de-OTTO.local>
2023-07-25 15:57:13 -0300

    incluindo operadores de identidade

---
+++ Condicionais/oper-identidade.js
@@ -0,0 +1,11 @@
+// identico "===" (mesmo conteudo e tipo);
+// oposto de identico "!=="
+
+const ehIdentico = 18 === 18;
+console.log(ehIdentico)
+
+// igual "==" (mesmo conteudo, apesar do tipo diferente)
+// nao é igual "!="
+
+const ehIgual = 18 == "18";
+console.log(ehIgual);
