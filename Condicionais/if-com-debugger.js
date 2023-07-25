cac39feed37388a771e3693241a36e3cf98c0fbc
OTTO <otto@MacBook-Air-de-OTTO.local>
2023-07-25 16:10:35 -0300

    Incluindo aprendizado com Debugger

---
+++ Condicionais/if-com-debugger.js
@@ -0,0 +1,9 @@
+const idade = 17;
+
+if (idade >= 18) {
+    console.log("É maior de idade");
+}
+console.log("Fim do código")
+
+
+// debugger é usado para verificação do código. Bom para estudar também.
