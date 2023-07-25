55500d30a56312c6e40a2988212d7a80f39a3a47
OTTO <otto@MacBook-Air-de-OTTO.local>
2023-07-25 16:17:47 -0300

    Operadores de igualdade

---
+++ Condicionais/If-else.js
@@ -0,0 +1,7 @@
+const idade = 17;
+
+if (idade >= 18) {
+    console.log("É maior de idade");
+} else {
+    console.log("É menor de idade")
+}
---
+++ Condicionais/oper-igualdade.js
@@ -0,0 +1,8 @@
+// quando tem dois "==" , pense que é uma pergunta
+const ehIgual = 16 == 16;
+console.log(ehIgual);
+
+// "!=" significa é diferente
+let x = 10;
+const ehDiferente = (x != 18);
+console.log(ehDiferente);
