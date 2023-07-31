128fc2b2372b2b855fe79c514070da0ee6e679ee
OTTO <otto@MacBook-Air-de-OTTO.local>
2023-07-31 18:37:46 -0300

    Objetos

---
+++ Desafios práticos/dados-corrompidos.js
@@ -0,0 +1,29 @@
+// PASSO A PASSO
+// 1. olhar cada um dos caracteres
+// 2. ver se pode o caractere em questao
+// 3. se for permitido, escrevo
+// 4. se nao for permitido, ignoro e olho proximo
+// 5. quando acabarem os caracteres, eh soh exibir o que eu escrevi
+
+// function solucao(stringCorrompida) {
+
+//     let dadoPurificado = '';
+//     for (let caractere of stringCorrompida) {
+//         if (caractere === '!' || caractere === '@' || caractere === '#' || caractere === '$' || caractere === '%' || caractere === '&' || caractere === '*' || caractere === '(' || caractere === ')' || caractere === '.');
+//     } else {
+//         dadoPurificado += caractere;
+//     }
+//     console.log(dadoPurificado);
+// }
+// function processData(input) {
+//     solucao(input);
+// }
+
+
+let dadoPurificado = '';
+
+for (let caractere of stringCorrompida) {
+    if (caractere === '!' || caractere === '@' || caractere === '#' || caractere === '$' || caractere === '%' || caractere === '&' || caractere === '*' || caractere === '(' || caractere === ')' || caractere === '.');
+} else {
+
+}
