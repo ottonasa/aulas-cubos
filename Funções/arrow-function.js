8d102cf23f35b441b6308cadcb292700b82c5ee1
OTTO <otto@MacBook-Air-de-OTTO.local>
2023-08-06 15:49:22 -0300

    Funcao com Arrow

---
+++ Funções/arrow-function.js
@@ -0,0 +1,19 @@
+// funcao com seta
+
+
+// DECLARACAO TRADICIONAL
+// function soma(x, y) {
+//     // codigo 
+//     const resultado = x + y;
+//     return resultado;
+
+// }
+
+// declaracao com arrow-function
+const soma = (x, y) => {
+    // codigo
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(5, 2));
---
+++ Funções/escopo-funcao.js
@@ -0,0 +1,7 @@
+// escopo é um trecho do codigo que é limitado por chaves({})
+
+function soma(x, y) {
+    return x + y;
+}
+
+console.log(soma(5, 8));
